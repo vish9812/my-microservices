748495d0c28267316cea5ea174a79b6200de9306
Vishal Choudhary <vish9812@gmail.com>
2021-09-15 23:17:25 +0530

    Add Ordering Feature with GetOrdersList query.

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository orderRepository;
+        private readonly IMapper mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            this.orderRepository = orderRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await orderRepository.GetOrdersByUserName(request.UserName);
+
+            return mapper.Map<List<OrdersVm>>(orders);
+        }
+    }
+}
